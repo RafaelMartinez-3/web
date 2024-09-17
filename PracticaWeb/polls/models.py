785132e94a5ab1eda1b7828a293c5974520e244d
RafaelMartinez-3 <s21120238@alumnos.itsur.edu.mx>
2024-09-16 21:50:22 -0600

    Subiendo Personalizacion de formularios

--- PracticaWeb/polls/models.py
+++ PracticaWeb/polls/models.py
@@ -1,9 +1,20 @@
+from datetime import datetime, timezone
 from django.db import models
+from django.contrib import admin
+
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
